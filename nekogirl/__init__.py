5179848e72093d27d4820e143d368ed295bfee21
mcwindy <chen_yuyang@outlook.com>
2020-12-20 15:29:16 +0800

    更新源码

---
+++ nekogirl/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+class Nekogirl():
+    def __init__(self, name):
+        self.name = name
+
+    def 名字(self):
+        return self.name
+
+    def 喵喵喵(self):
+        print('喵喵喵')
+
+    def 变(self):
+        print('变好了')
+
+
+if __name__ == '__main__':
+    my_nekogirl = Nekogirl('sweety')
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='nekogirl',
+    version='0.0.1',
+    author='mcwindy',
+    author_email='chen_yuyang@outlook.com',
+    url='https://github.com/mcwindy',
+    description='这个代码提供了猫娘',
+    packages=['nekogirl'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'Nekogirl=nekogirl:Nekogirl'
+        ]
+    }
+)
