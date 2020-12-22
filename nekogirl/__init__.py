ed5a817c98ef410911a434c85f289e3ed0a35584
LaoshuBaby <keaitianxinmail@qq.com>
2020-12-22 23:40:52 +0800

    Fix Chinese Character Bug And add NEKOGIRL

--- nekogirl/__init__.py
+++ nekogirl/__init__.py
@@ -1,6 +1,6 @@
 class Nekogirl:
-    states = {'猫猫', '猫娘'}
-	emotion = {'开心'，'沮丧','生气','发情'}
+    state = {'猫猫', '猫娘'}
+	emotion = {'开心','沮丧','生气','发情'}
 
     def __init__(self, name, state='猫猫'):
         self.name = name
@@ -14,6 +14,8 @@ class Nekogirl:
         if self.state == '猫猫':
             if random.random() < 0.5:  # 现在猫猫有概率不理你
                 print('喵喵喵')
+			#if self.emotion='发情':
+			#    print('呜喵呜喵')
         else:
             import time
             hour = int(time.asctime().split(' ')[3].split(':')[0])
@@ -38,11 +40,19 @@ class Nekogirl:
         -(((---(((--------♥ ''')
         else:
             print('还没有搞好TuT,可以发issue提供字符画或者照片')
+
+#		   |\__/,|    
+#        _.(o o  )_  (`♥ 
+#          ◟_ω_ ◞    ) )
+#        -{       }  ) ) 
+#          (     )  ))
+#           || || ︶
+#           ω   ω
 			
 	def 咬人(self):
 	    if self.emotion == '生气':
-		    if random.random() < 0.3:
-		        print('猫猫生气了！会咬人的！')
+	        if random.random() < 0.3:
+	            print('猫猫生气了！会咬人的！')
 
 
 if __name__ == '__main__':
